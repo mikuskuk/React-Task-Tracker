5bd9516e8093f11d72fa044c374814913e4dcf0f
Mikus <109518376+mikuskuk@users.noreply.github.com>
2023-02-03 14:39:23 +0100

    Add files via upload

---
+++ build/precache-manifest.58bbe6353cf62fbfb098b5fc366e0645.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcb684982bf6e60d2c5d",
+    "url": "/static/css/main.f29893a4.chunk.css"
+  },
+  {
+    "revision": "fcb684982bf6e60d2c5d",
+    "url": "/static/js/main.31f0b849.chunk.js"
+  },
+  {
+    "revision": "b11fdbbdaa4e7cf1295b",
+    "url": "/static/js/runtime~main.5f042030.js"
+  },
+  {
+    "revision": "960b1f59d28c6dba62fa",
+    "url": "/static/js/2.95c8dca6.chunk.js"
+  },
+  {
+    "revision": "bd2db5e315108ebec595",
+    "url": "/static/js/3.5b0ae9b5.chunk.js"
+  },
+  {
+    "revision": "dcc5a0dd00c174e8c494530fd91a05bb",
+    "url": "/index.html"
+  }
+];
